f1ccc96b3d0eb13b48fa47534715de716a50adf0
ebShare <127939817+ebShare@users.noreply.github.com>
2024-06-27 08:55:37 +0200

    Création de l'entité lecon

---
+++ src/Entity/Lecon.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\LeconRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: LeconRepository::class)]
+class Lecon
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $titre = null;
+
+    #[ORM\Column]
+    private ?int $code = null;
+
+    #[ORM\Column]
+    private ?int $heures = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitre(): ?string
+    {
+        return $this->titre;
+    }
+
+    public function setTitre(string $titre): static
+    {
+        $this->titre = $titre;
+
+        return $this;
+    }
+
+    public function getCode(): ?int
+    {
+        return $this->code;
+    }
+
+    public function setCode(int $code): static
+    {
+        $this->code = $code;
+
+        return $this;
+    }
+
+    public function getHeures(): ?int
+    {
+        return $this->heures;
+    }
+
+    public function setHeures(int $heures): static
+    {
+        $this->heures = $heures;
+
+        return $this;
+    }
+}
