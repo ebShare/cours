e4ccdca56b5786212d04dd0efdc4b0e07fa3bef9
ebShare <127939817+ebShare@users.noreply.github.com>
2024-06-27 09:52:49 +0200

    Partie 2 : CRUD des lecons - Formulaire pour créer une lecon (construction, rendu et traitement)

--- src/Controller/LeconController.php
+++ src/Controller/LeconController.php
@@ -8,6 +8,9 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Component\HttpFoundation\Request;
+use App\Entity\Lecon;
+use App\Form\LeconType;
+use Doctrine\ORM\EntityManagerInterface;
 
 class LeconController extends AbstractController
 {
@@ -24,5 +27,24 @@ class LeconController extends AbstractController
         return $this->render('pages/lecon/index.html.twig', [
             'lecons' => $lecons,
         ]);
+    }
+    #[Route('/lecon/nouveau', 'lecon_new', methods: ['GET', 'POST'])]
+    public function new(
+        Request $request,
+        EntityManagerInterface $manager
+    ): Response {
+        $lecon = new Lecon();
+        $form = $this->createForm(LeconType::class, $lecon);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $lecon = $form->getData();
+            $manager->persist($lecon);
+            $manager->flush();
+
+            return $this->redirectToRoute('app_lecon');
+        }
+
+        return $this->render('pages/lecon/new.html.twig', ['form' => $form,]);
     }
 }
---
+++ src/Form/LeconType.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Lecon;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class LeconType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('titre')
+            ->add('code')
+            ->add('heures')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Lecon::class,
+        ]);
+    }
+}
